201fd4523b187248fe7ddc1d2f221a4b9b3a579b
Shogil pp <shogilpp@Shogils-MacBook-Air.local>
2022-04-23 11:11:54 +0530

    update the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(MyApp());
-}
+// void main(List<String> args) {
+//   runApp(MyApp());
+// }
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first project'),
+        ),
+        body: Text('This is the first text'),
+      ),
+    );
   }
 }
