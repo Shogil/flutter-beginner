f4846a9158ca6fbe0bb8b9fa8f5c9548ac2a5a67
Shogil pp <shogilpp@Shogils-MacBook-Air.local>
2022-04-23 12:01:10 +0530

    update the files

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,20 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'what\'s your favorite color?',
+      'what\'s your favorite color?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first project'),
         ),
-        body: Text('This is the first text'),
+        body: Column(
+          children: [],
+        ),
       ),
     );
   }
