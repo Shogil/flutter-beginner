0ee0c1bb5c7942012ae509ef864b98dc07b9cacf
Shogil pp <shogilpp@Shogils-MacBook-Air.local>
2022-04-24 20:04:32 +0530

    update the main file

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,28 @@ import 'package:flutter/material.dart';
 // }
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State {
+  var questionIndex = 0;
   void answerQuestion() {
-    print('Answer chosen!');
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
     var questions = [
       'what\'s your favorite color?',
-      'what\'s your favorite color?'
+      'what\'s your favorite color?' 'what\'s your favorite color?'
     ];
     return MaterialApp(
       home: Scaffold(
@@ -24,7 +35,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The question!'),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: answerQuestion,
