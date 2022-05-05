d3ea7ac30f5d7b9799d287df606dd624472ae77b
shogil <shogilbabu@gmail.com>
2022-05-05 11:44:15 +0530

    update the result

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class _MyAppState extends State {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
